34de512725b7aea2cf8d3f08edf62799277b4229
Alex Finnarn <alexfinnarn@gmail.com>
2020-03-12 12:54:33 -0400

    changes

--- src/stories/Login.stories.js
+++ src/stories/Login.stories.js
@@ -4,10 +4,6 @@ import { Button } from '../components/Button';
 
 export default {
   title: 'Molecules|Login Form',
-  component: Text,
-  // parameters: {
-  //   assets: ['https://xd.adobe.com/embed/346520e5-a8ad-4e03-4f64-b3afb2a77ff2-4254/'],
-  // },
 };
 
 export const Default = () => (
