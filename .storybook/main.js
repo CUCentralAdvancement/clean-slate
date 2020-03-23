536bee0960e6509ff2bc77e959edfbcbf72e6ab3
Alex Finnarn <alexfinnarn@gmail.com>
2020-03-23 20:24:36 +0000

    Feature/18

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,4 @@
 module.exports = {
-  stories: ['../src/**/*.stories.(js|mdx)'],
-  addons: [
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-actions',
-    '@storybook/addon-knobs',
-    '@storybook/addon-links',
-    '@storybook/addon-a11y',
-    '@storybook/addon-storysource',
-    '@storybook/addon-design-assets',
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        configureJSX: true,
-      },
-    },
-  ],
+  stories: ['../stories/**/*.stories.js'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
 };
