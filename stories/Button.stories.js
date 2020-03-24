3d64fbd7348629c1eeb29e23f7033889c7a38f50
Alex Finnarn <alexfinnarn@gmail.com>
2020-03-24 12:16:45 -0400

    add Rebass and use Button component

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -1,16 +1,44 @@
 import React from 'react';
-import { text } from '@storybook/addon-knobs';
+import { text, select, number } from '@storybook/addon-knobs';
 
-import { Button } from '../src/components/Button';
+import { BaseButton } from '../src/components/Button';
 
 export default {
   title: 'Atoms|Button',
   component: Button,
 };
 
+const paddingOptions = {
+  range: true,
+  min: 1,
+  max: 9,
+  step: 1,
+};
+
+function Button(props) {
+  return <BaseButton {...props} mr={2}></BaseButton>;
+}
+
 export const allText = () => (
   <div>
-    <Button>{text('Button Text', 'Click Me!')}</Button>
+    <span>Knobs: </span>
+    <Button
+      variant={select('Variants', { primary: 'primary', secondary: 'secondary', outline: 'outline' }, 'primary')}
+      sx={{
+        fontSize: 1,
+        textTransform: 'uppercase',
+        borderRadius: 99999,
+      }}
+      mb={2}
+      p={number('Padding', 2, paddingOptions)}
+    >
+      {text('Button Text', 'Click Me!')}
+    </Button>
+    <hr />
+    <Button variant="primary">Primary</Button>
+    <Button variant="secondary">Secondary</Button>
+    <Button variant="outline">Outline</Button>
+
     <Button isDisabled>Disabled</Button>
   </div>
 );
