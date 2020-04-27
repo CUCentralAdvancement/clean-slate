e8957d895559033fd1cc0dc7643b1e085a7f5cb4
Alex Finnarn <alexfinnarn@gmail.com>
2020-04-27 14:14:23 -0400

    remove Formik dependency

--- src/components/SelectInput.js
+++ src/components/SelectInput.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useField } from 'formik';
 import { Label, Select } from 'theme-ui';
 
-export default function SelectInput({ label, ...props }) {
-  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-  // which we can spread on <input> and also replace ErrorMessage entirely.
-  const [field, meta] = useField(props);
+export default function SelectInput({ label, error, ...props }) {
   return (
     <>
-      <Label htmlFor={props.id || props.name}>{label}</Label>
-      <Select {...field} {...props} color={meta.touched && meta.error ? 'colors.error' : 'inherit'} bg="background" />
-      {meta.touched && meta.error ? (
-        <Text mt={1} ml={2} color="error">
-          {meta.error}
-        </Text>
-      ) : null}
+      {label && <Label htmlFor={props.id || props.name}>{label}</Label>}
+      <Select {...props} color={error ? 'colors.error' : 'inherit'} bg="background" />
     </>
   );
 }
@@ -25,8 +16,13 @@ SelectInput.propTypes = {
    * URL where the button goes once clicked.
    */
   label: PropTypes.string,
+  /**
+   * Error message, if any.
+   */
+  error: PropTypes.string,
 };
 
 SelectInput.defaultProps = {
   label: '',
+  error: '',
 };
--- stories/recipes/SearchForm.js
+++ stories/recipes/SearchForm.js
@@ -1,91 +1,94 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Formik, Form } from 'formik';
-import * as Yup from 'yup';
 import { Flex, Box } from 'theme-ui';
+import { useForm } from 'react-hook-form';
 
 import TextInput from '../../src/components/TextInput';
 import BaseButton from '../../src/components/BaseButton';
 import SelectInput from '../../src/components/SelectInput';
 
-export default function SearchForm({ submitHandler, resetHandler, ...props }) {
+export default function SearchForm({ submitHandler, resetHandler }) {
+  // validationSchema={Yup.object({
+  //   search: Yup.string()
+  //     .min(3, 'Search query must be at least 3 characters long')
+  //     .required('Search query is required'),
+  // })}
+  // register({ required: true, maxLength: 20 })
+
+  const { register, handleSubmit, setValue } = useForm();
+
+  React.useEffect(() => {
+    register({ name: 'campus' });
+    register({ name: 'interest' });
+    register({ name: 'fundType' });
+    register({ name: 'search' });
+  }, [register]);
+
+  const handleSelect = (e, name) => {
+    setValue(name, e.target.value);
+    // console.log(name, e.target.value);
+  };
+
+  const handleTextInput = (e, name) => {
+    setValue(name, e.target.value);
+    // console.log(name, option);
+  };
+
   return (
     <>
-      <Formik
-        initialValues={{
-          search: '',
-          campus: 'All',
-          interest: 'All',
-          fundType: 'All',
-        }}
-        // validationSchema={Yup.object({
-        //   search: Yup.string()
-        //     .min(3, 'Search query must be at least 3 characters long')
-        //     .required('Search query is required'),
-        // })}
-        onSubmit={(values, { setSubmitting }) => {
-          submitHandler(values);
-          setSubmitting(false);
-        }}
-      >
-        {({ resetForm, values }) => (
-          <Form>
-            <Flex mx={-2} mb={3} flexDirection={['column', 'row', 'row']}>
-              <Box width={[1, 1, 1 / 3]} px={2}>
-                <SelectInput name="campus">
-                  <option value="All">All Campuses</option>
-                  <option value="1046">CU Anschutz</option>
-                  <option value="904">CU Boulder</option>
-                  <option value="1100">CU Denver</option>
-                  <option value="1134">UCCS</option>
-                  <option value="1150">CU System</option>
-                </SelectInput>
-              </Box>
-              <Box width={[1, 1, 1 / 3]} px={2}>
-                <SelectInput name="interest">
-                  <option value="All">All Interests</option>
-                  <option value="950">Alumni Programs</option>
-                  <option value="917">Arts &amp; Culture</option>
-                  <option value="953">Athletics &amp; Recreation</option>
-                  <option value="958">Community &amp; Society</option>
-                  <option value="983">Enterprise &amp; Entrepreneurship</option>
-                  <option value="971">Health &amp; Wellness</option>
-                  <option value="907">Scholarships &amp; Student Success</option>
-                  <option value="910">Science, Research &amp; Innovation</option>
-                </SelectInput>
-              </Box>
-              <Box width={[1, 1, 1 / 3]} px={2}>
-                <SelectInput name="fundType">
-                  <option value="All">Fund Type</option>
-                  <option value="908">Academic Program Funds</option>
-                  <option value="1043">Endowed Funds</option>
-                  <option value="946">Memorial Funds</option>
-                </SelectInput>
-              </Box>
-            </Flex>
-            <Flex mx={-2} mb={3} flexDirection={['column', 'row', 'row']}>
-              <Box width={[1, 1, 3 / 4]} px={2}>
-                <TextInput name="search" type="text" />
-              </Box>
-              <Box width={[1, 1, 1 / 4]} px={2} pt={[2, 0, 0]}>
-                <BaseButton variant="button.secondary" type="submit" mr={2}>
-                  Search
-                </BaseButton>
-                <BaseButton
-                  variant="button.secondary"
-                  type="reset"
-                  onClick={() => {
-                    resetHandler();
-                    resetForm();
-                  }}
-                >
-                  Reset
-                </BaseButton>
-              </Box>
-            </Flex>
-          </Form>
-        )}
-      </Formik>
+      <form onSubmit={handleSubmit(submitHandler)}>
+        <Flex sx={{ mx: -2, mb: [0, 3, 3], flexDirection: ['column', 'row', 'row'] }}>
+          <Box sx={{ width: '100%', px: 2, pt: 2 }}>
+            <SelectInput name="campus" onChange={(e) => handleSelect(e, 'campus')}>
+              <option value="All">All Campuses</option>
+              <option value="1046">CU Anschutz</option>
+              <option value="904">CU Boulder</option>
+              <option value="1100">CU Denver</option>
+              <option value="1134">UCCS</option>
+              <option value="1150">CU System</option>
+            </SelectInput>
+          </Box>
+          <Box sx={{ width: '100%', px: 2, pt: 2 }}>
+            <SelectInput name="interest" onChange={(e) => handleSelect(e, 'interest')}>
+              <option value="All">All Interests</option>
+              <option value="950">Alumni Programs</option>
+              <option value="917">Arts &amp; Culture</option>
+              <option value="953">Athletics &amp; Recreation</option>
+              <option value="958">Community &amp; Society</option>
+              <option value="983">Enterprise &amp; Entrepreneurship</option>
+              <option value="971">Health &amp; Wellness</option>
+              <option value="907">Scholarships &amp; Student Success</option>
+              <option value="910">Science, Research &amp; Innovation</option>
+            </SelectInput>
+          </Box>
+          <Box sx={{ width: '100%', px: 2, pt: 2 }}>
+            <SelectInput name="fundType" onChange={(e) => handleSelect(e, 'fundType')}>
+              <option value="All">Fund Type</option>
+              <option value="908">Academic Program Funds</option>
+              <option value="1043">Endowed Funds</option>
+              <option value="946">Memorial Funds</option>
+            </SelectInput>
+          </Box>
+        </Flex>
+        <Flex sx={{ mx: -2, mb: 3, flexDirection: ['column', 'row', 'row'] }}>
+          <Box sx={{ width: ['100%', '70%'], px: 2, pt: [2, 0, 0] }}>
+            <TextInput name="search" type="text" onChange={(e) => handleTextInput(e, 'search')} />
+          </Box>
+          <Box sx={{ width: ['100%', '30%'], px: 2, pt: [2, 0, 0] }}>
+            <BaseButton variant="button.secondary" type="submit" mr={2}>
+              Search
+            </BaseButton>
+            <BaseButton
+              variant="button.secondary"
+              type="reset"
+              onClick={() => {
+                resetHandler();
+              }}
+            >
+              Reset
+            </BaseButton>
+          </Box>
+        </Flex>
+      </form>
     </>
   );
 }
