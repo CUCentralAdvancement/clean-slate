7d3b88cf76998d99287e0dfe94717b6bf4e312d1
Alex Finnarn <alexfinnarn@gmail.com>
2020-05-05 13:28:04 -0400

    add react select to project

--- src/components/SelectInput.js
+++ src/components/SelectInput.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Label, Select } from 'theme-ui';
+import { Label } from 'theme-ui';
+import Select from 'react-select';
+
+const customStyles = {
+  indicatorsContainer: (provided, state) => ({
+    ...provided,
+    backgroundColor: '#000',
+  }),
+  indicatorSeparator: (provided, state) => ({
+    ...provided,
+    backgroundColor: 'inherit',
+  }),
+};
 
 export default function SelectInput({ label, error, ...props }) {
   return (
     <>
       {label && <Label htmlFor={props.id || props.name}>{label}</Label>}
-      <Select {...props} color={error ? 'colors.error' : 'inherit'} bg="background" />
+      <Select {...props} styles={customStyles} color={error ? 'colors.error' : 'inherit'} bg="background" />
     </>
   );
 }
